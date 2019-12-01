4ecbedd7e34cf9a27b20b117db3a20517f3fbb8e
anmol sharma <anmolsharma433@gmail.com>
2019-12-01 13:27:54 -0500

    layout fixed

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.taxcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText fname;
+    private EditText lanme;
+    private EditText age;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
